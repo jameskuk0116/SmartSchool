5a4ddc568e16239eb4ffcef77cf30fe51bab4bfd
jamesGu <790032475@qq.com>
2015-04-24 14:54:23 +0800

    添加BmobSDK

--- SmartSchool/main.m
+++ SmartSchool/main.m
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-
+#import <BmobSDK/Bmob.h>
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        NSString *appKey = @"d04f284cf6885fb257571779535fc49a";
+        [Bmob registerWithAppKey:appKey];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
