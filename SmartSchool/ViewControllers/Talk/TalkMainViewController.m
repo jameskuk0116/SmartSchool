527e0de3bfe706b87ccef5f7925d1d4b94360939
jamesGu <790032475@qq.com>
2015-05-06 18:06:53 +0800

    添加客服

--- SmartSchool/ViewControllers/Talk/TalkMainViewController.m
+++ SmartSchool/ViewControllers/Talk/TalkMainViewController.m
@@ -8,6 +8,7 @@
 
 #import "TalkMainViewController.h"
 #import "RCIM.h"
+#import "RCChatViewController.h"
 
 @interface TalkMainViewController ()
 
@@ -24,6 +25,13 @@
     
     [RCIM connectWithToken:@"ZtaxrZfT994dL0u9V8l2gIviFRF+0gv+xIt4Zy8jC33yU0suoeqp8jR5Z2D1v7x5GZe5sr4/Wx74TrC5RmLOZaS4NtEZTpcu" completion:^(NSString *userId) {
         // 此处处理连接成功。
+        RCChatViewController *chatViewController = [[RCIM sharedRCIM]createPrivateChat:@"1" title:@"自问自答" completion:^(){
+            // 创建 ViewController 后，调用的 Block，可以用来实现自定义行为。
+        }];
+        
+        // 把单聊视图控制器添加到导航栈。
+        [self.navigationController pushViewController:chatViewController animated:YES];
+        
         NSLog(@"Login successfully with userId: %@.", userId);
     } error:^(RCConnectErrorCode status) {
         // 此处处理连接错误。
@@ -42,7 +50,14 @@
 }
 
 -(void)leftBarButtonItemPressed:(UIButton *)sender{
-
+    // 创建客服聊天视图控制器。
+    RCChatViewController *chatViewController = [[RCIM sharedRCIM]createCustomerService:@"KEFU1430906042741" title:@"在线客服" completion:^(){
+        // 创建 ViewController 后，调用的 Block，可以用来实现自定义行为。
+    }];
+    
+    // 把客服聊天视图控制器添加到导航栈。
+    chatViewController.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:chatViewController animated:YES];
 }
 /*
 #pragma mark - Navigation
